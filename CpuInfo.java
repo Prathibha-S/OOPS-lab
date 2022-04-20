609360b0623e7dd23c8516c94d7e6a29bcedb7ab
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-04-20 14:47:37 +0530

    Update CpuInfo.java

--- CpuInfo.java
+++ CpuInfo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Cpu{
 	double price;
 	class Processor{
@@ -20,7 +22,23 @@ public class CpuInfo {
 		Cpu cp=new Cpu();
 		Cpu.Processor p=cp.new Processor();
 		Cpu.Ram r=new Cpu.Ram();
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the price of the CPU");
+		cp.price=s.nextDouble();
+		System.out.println("Enter the cores of the processor");
+		p.cores=s.nextDouble();
+		System.out.println("Enter the manufacturer of the processor");
+		p.manufacturer=s.next();
+		System.out.println("Enter the memory");
+		r.memory=s.nextDouble();
+		System.out.println("Enter the manufacturer");
+		r.manufacturer=s.next();
+		System.out.println("The price of the CPU is "+cp.price);
+		System.out.println("The cores of the processor is "+p.cores);
+		System.out.println("The manufacturer of the processor is "+p.manufacturer);
 		System.out.println("The Processor Cache is "+p.Cache());
+		System.out.println("The memory is "+r.memory);
+		System.out.println("The manufacturer is "+r.manufacturer);
 		System.out.println("The RAM Clock speed is "+r.ClockSpeed());
 	}
 
