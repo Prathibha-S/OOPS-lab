ae71876da13463b5d4c468d42bb9c0a63d4b7813
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-04-25 15:40:36 +0530

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -1,27 +1,22 @@
 import java.util.Arrays;
 import java.util.*;
 public class Sort{
-
 	public static void main(String[] args) {
 		int n1,ch;
 		boolean t=true;
 	    Scanner r=new Scanner(System.in);
-		
 		n1=2;
-		String a[]=new String[n1];
-	 		
+		String a[]=new String[n1];	
 		System.out.println("1.Insert String:");
 		System.out.println("2.Sort function:");
-		System.out.println("3.sort");
-		System.out.println("4.display:");
+		System.out.println("3.Sort");
+		System.out.println("4.Display:");
 		System.out.println("5.Exit");
 		while(t)
 		{
 			System.out.println("Enter your choice:");
 			ch=r.nextInt();
-			
 			switch(ch) {
-			
 			case 1 : 
 				System.out.println("Enter the no of strings:");
 				n1=r.nextInt();
@@ -46,20 +41,15 @@ public class Sort{
 							a[j]=temp;
 						}
 					}
-					
 				}
 				break;
 			case 4 : 
 				System.out.println(Arrays.toString(a));
 				break;
-			
 			case 5 :
 				t=false;
 				break;
-				
 			}
 		}
-		
 	}
-
 }
